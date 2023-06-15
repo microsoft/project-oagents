2521f310f76d06e55872c2671722ed7e9faba6a8
Kosta Petan <kostapetan@gmail.com>
2023-06-15 11:22:53 +0000

    call the seed project in the postCreateCommand script

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -2,4 +2,6 @@
 
 curl -k https://localhost:8081/_explorer/emulator.pem > ~/emulatorcert.crt
 sudo cp ~/emulatorcert.crt /usr/local/share/ca-certificates/
-sudo update-ca-certificates 
+sudo update-ca-certificates
+sleep 5
+dotnet run --project util/seed-memory/seed-memory.csproj
