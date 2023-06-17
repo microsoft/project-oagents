cd459480ad5fba0097326ebdb30476d5d6a07f79
Kosta Petan <kostapetan@gmail.com>
2023-06-17 19:28:49 +0000

    gh issues WIP

--- sk-azfunc-server/ExecuteFunctionEndpoint.cs
+++ sk-azfunc-server/ExecuteFunctionEndpoint.cs
@@ -6,6 +6,7 @@ using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Orchestration;
 using Models;
+using Octokit;
 using skills;
 
 public class ExecuteFunctionEndpoint
@@ -59,6 +60,82 @@ public class ExecuteFunctionEndpoint
 
             return await CreateResponseAsync(requestData, HttpStatusCode.BadRequest, new ErrorResponse() { Message = $"Invalid request body." }).ConfigureAwait(false);
         }
+    }
+    
+    [Function("GithubWebhook")]
+     public async Task<HttpResponseData> GithubWebhook(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "github")]
+        HttpRequestData requestData,
+        FunctionContext executionContext)
+    {
+        
+        var ghClient = await GetGitHubClient("key.pem", 1234, 1234);
+        var issueCommentResponse = await ghClient.Issue.Comment.Create("owner", "repo", 1, "Hello from my GitHubApp Installation!");
+        var issueEvent = await JsonSerializer.DeserializeAsync<IssueEvent>(requestData.Body, s_jsonOptions).ConfigureAwait(false);
+       
+        // try
+        // {
+        //     // Get the payload
+        //     // check what type it is
+        //     // handle each type
+        //     // // issue opened
+        //         // read the issue object, the body has the input, the label has the skill
+        //     // // issue-comment created - edited
+        //        // this is a follow up of the previous action, the body and the label are important
+        //        // if the label is a Skill, take the body and run the skill, post the result as new comment
+        //        // if the body is Yes/No, re-run the previous skill?
+        //     // run the skill and return the output as a new issue comment
+        //     // deal with the side effects
+            
+            
+        //     var skillConfig = SemanticFunctionConfig.ForSkillAndFunction(skillName, functionName);
+        //     var function = _kernel.CreateSemanticFunction(skillConfig.PromptTemplate, skillConfig.Name, skillConfig.SkillName,
+        //                                                skillConfig.Description, skillConfig.MaxTokens, skillConfig.Temperature,
+        //                                                skillConfig.TopP, skillConfig.PPenalty, skillConfig.FPenalty);
+
+        //     var context = new ContextVariables();
+        //     foreach (var v in functionRequest.Variables)
+        //     {
+        //         context.Set(v.Key, v.Value);
+        //     }
+
+        //     var result = await this._kernel.RunAsync(context, function).ConfigureAwait(false);
+
+        //     return await CreateResponseAsync(requestData, HttpStatusCode.OK, new ExecuteFunctionResponse() { Response = result.ToString() }).ConfigureAwait(false);
+        // }
+        // catch (Exception ex)
+        // {
+        //     // Log the contents of the request
+        //     var requestBody = await new StreamReader(requestData.Body).ReadToEndAsync();
+        //     Console.WriteLine($"Failed to deserialize request body: {requestBody}. Exception: {ex}");
+
+        //     return await CreateResponseAsync(requestData, HttpStatusCode.BadRequest, new ErrorResponse() { Message = $"Invalid request body." }).ConfigureAwait(false);
+        // }
+        return await CreateResponseAsync(requestData,HttpStatusCode.OK, new ExecuteFunctionResponse() { Response = "ok" }).ConfigureAwait(false);
+    }
+
+    private static async Task<GitHubClient> GetGitHubClient(string keyPath,int appId,  int installationId)
+    {
+        // Use GitHubJwt library to create the GitHubApp Jwt Token using our private certificate PEM file
+        var generator = new GitHubJwt.GitHubJwtFactory(
+            new GitHubJwt.FilePrivateKeySource(keyPath),
+            new GitHubJwt.GitHubJwtFactoryOptions
+            {
+                AppIntegrationId = appId, // The GitHub App Id
+                ExpirationSeconds = 600 // 10 minutes is the maximum time allowed
+            }
+        );
+
+        var jwtToken = generator.CreateEncodedJwtToken();
+        var appClient = new GitHubClient(new ProductHeaderValue("SK-DEV-APP"))
+        {
+            Credentials = new Credentials(jwtToken, AuthenticationType.Bearer)
+        };
+        var response = await appClient.GitHubApps.CreateInstallationToken(installationId);
+        return new GitHubClient(new ProductHeaderValue($"SK-DEV-APP-Installation{installationId}"))
+        {
+            Credentials = new Credentials(response.Token)
+        };
     }
 
     private static async Task<HttpResponseData> CreateResponseAsync(HttpRequestData requestData, HttpStatusCode statusCode, object responseBody)
