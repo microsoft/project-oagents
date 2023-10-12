135efe2fa69252e43ec2bb129d339ec96bc8aa87
Andre Hofmeister <9199345+HofmeisterAn@users.noreply.github.com>
2023-10-12 17:05:07 +0200

    chore: Update Testcontainers for .NET

--- src/apps/cli/SandboxSkill.cs
+++ src/apps/cli/SandboxSkill.cs
@@ -1,38 +1,49 @@
+using System.Text;
 using DotNet.Testcontainers.Builders;
-using Microsoft.SemanticKernel.SkillDefinition;
+using DotNet.Testcontainers.Configurations;
+using DotNet.Testcontainers.Containers;
 
 public class SandboxSkill
 {
-    public async Task<string> RunInAlpineAsync(string input)
+    public Task<string> RunInAlpineAsync(string input)
     {
-        return await RunInContainer(input, "alpine");
+        return RunInContainer(input, "alpine:3.18");
     }
 
-    public async Task<string> RunInDotnetAlpineAsync(string input)
+    public Task<string> RunInDotnetAlpineAsync(string input)
     {
-        return await RunInContainer(input, "mcr.microsoft.com/dotnet/sdk:7.0");
+        return RunInContainer(input, "mcr.microsoft.com/dotnet/sdk:7.0-alpine");
     }
 
-    private async Task<string> RunInContainer(string input, string image)
+    private static async Task<string> RunInContainer(string input, string image)
     {
-        var tempScriptFile = $"{Guid.NewGuid().ToString()}.sh";
-        var tempScriptPath = $"./output/{tempScriptFile}";
-        await File.WriteAllTextAsync(tempScriptPath, input);
-        Directory.CreateDirectory(Path.Combine(Directory.GetCurrentDirectory(),"output", "src"));
+        var tempScriptFile = Path.ChangeExtension(Guid.NewGuid().ToString(), "sh");
+
+        var srcDirectoryPath = Path.Combine(Directory.GetCurrentDirectory(), "output", "src");
+        _ = Directory.CreateDirectory(srcDirectoryPath);
+
         var dotnetContainer = new ContainerBuilder()
-                            .WithName(Guid.NewGuid().ToString("D"))
-                            .WithImage(image)
-                            .WithBindMount(Path.Combine(Directory.GetCurrentDirectory(),"output", "src"), "/src")
-                            .WithBindMount(Path.Combine(Directory.GetCurrentDirectory(), tempScriptPath), $"/src/{tempScriptFile}")
-                            .WithWorkingDirectory("/src")
-                            .WithCommand("sh", tempScriptFile)
-                            .Build();
+            .WithImage(image)
+            .WithBindMount(srcDirectoryPath, "/src")
+            .WithResourceMapping(Encoding.Default.GetBytes(input), $"/src/{tempScriptFile}")
+            .WithWaitStrategy(Wait.ForUnixContainer().AddCustomWaitStrategy(new ScriptCompleted()))
+            .WithWorkingDirectory("/src")
+            .WithEntrypoint("/bin/sh")
+            .WithCommand(tempScriptFile)
+            .Build();
 
         await dotnetContainer.StartAsync()
-                            .ConfigureAwait(false);
-        // Cleanup
-        File.Delete(tempScriptPath);
-        File.Delete(Path.Combine(Directory.GetCurrentDirectory(), "output", "src", tempScriptFile));
-        return "";
+            .ConfigureAwait(false);
+
+        File.Delete(Path.Combine(srcDirectoryPath, tempScriptFile));
+        return string.Empty;
+    }
+
+    private sealed class ScriptCompleted : IWaitUntil
+    {
+        public Task<bool> UntilAsync(IContainer container)
+        {
+            return Task.FromResult(TestcontainersStates.Exited.Equals(container.State));
+        }
     }
 }
