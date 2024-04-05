e3bb8fbae2a1db492d96280b369c0ae38eefe2a0
Mohamed Ali <antivirus.cs@gmail.com>
2024-04-05 15:43:48 +0300

    [WorkflowApp] Switch to Elsa Studio (Blazor Server)

--- samples/WorkflowsApp/Program.cs
+++ samples/WorkflowsApp/Program.cs
@@ -3,8 +3,43 @@ using Elsa.EntityFrameworkCore.Modules.Management;
 using Elsa.EntityFrameworkCore.Modules.Runtime;
 using Elsa.Extensions;
 using Elsa.SemanticKernel;
+using Elsa.Studio.Core.BlazorServer.Extensions;
+using Elsa.Studio.Dashboard.Extensions;
+using Elsa.Studio.Extensions;
+using Elsa.Studio.Login.BlazorServer.Extensions;
+using Elsa.Studio.Login.HttpMessageHandlers;
+using Elsa.Studio.Shell.Extensions;
+using Elsa.Studio.Workflows.Designer.Extensions;
+using Elsa.Studio.Workflows.Extensions;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Register Razor services.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor(options =>
+{
+    // Register the root components.
+    options.RootComponents.RegisterCustomElsaStudioElements();
+});
+
+// Register shell services and modules.
+builder.Services.AddCore();
+builder.Services.AddShell(options => builder.Configuration.GetSection("Shell").Bind(options));
+builder.Services.AddRemoteBackend(
+    elsaClient => elsaClient.AuthenticationHandler = typeof(AuthenticatingApiHttpMessageHandler),
+    options => builder.Configuration.GetSection("Backend").Bind(options));
+builder.Services.AddLoginModule();
+builder.Services.AddDashboardModule();
+builder.Services.AddWorkflowsModule();
+
+// Configure SignalR.
+builder.Services.AddSignalR(options =>
+{
+    // Set MaximumReceiveMessageSize:
+    options.MaximumReceiveMessageSize = 5 * 1024 * 1000; // 5MB
+});
+
 
 // Add services to the container.
 builder.Services.AddElsa(elsa =>
@@ -18,7 +53,10 @@ builder.Services.AddElsa(elsa =>
     elsa.UseWorkflowsApi();
 
     // Add services for HTTP activities and workflow middleware.
-    elsa.UseHttp();
+    elsa.UseHttp(http =>
+    {
+        http.ConfigureHttpOptions = options => builder.Configuration.GetSection("Http").Bind(options);
+    });
     
     // Configure identity so that we can create a default admin user.
     elsa.UseIdentity(identity =>
@@ -30,25 +68,38 @@ builder.Services.AddElsa(elsa =>
     // Use default authentication (JWT + API Key).
     elsa.UseDefaultAuthentication(auth => auth.UseAdminApiKey());
 
+    elsa.UseRealTimeWorkflows();
+
     // Add Semantic Kernel skill.
     elsa.AddActivity<SemanticKernelSkill>();
 
 });
+builder.Services.AddControllers();
+builder.Services.AddCors(cors =>
+    cors.AddDefaultPolicy(policy => {
+        policy.AllowAnyHeader();
+        policy.AllowAnyOrigin();
+        policy.AllowAnyMethod();
+    })
+);
 
 // Add dynamic Activity Provider for SK skills.
 builder.Services.AddActivityProvider<SemanticKernelActivityProvider>();
 
 // Add Razor pages.
-builder.Services.AddRazorPages();
+builder.Services.AddRazorPages(options => options.Conventions.ConfigureFilter(new IgnoreAntiforgeryTokenAttribute()));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseHttpsRedirection();
+app.UseRouting();
+app.UseCors();
 app.UseStaticFiles();
 app.UseAuthentication();
 app.UseAuthorization();
 app.UseWorkflowsApi();
 app.UseWorkflows();
-app.MapRazorPages();
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
 app.Run();
