637e0977cb3cf74a17e401128e7e07e5f2e09b77
Carlos Garcia Lalicata <crgarcia@live.com.ar>
2024-04-29 12:03:08 +0200

    cleaning to prep for push to upstream

--- samples/marketing/src/frontend/src/app/layout.tsx
+++ samples/marketing/src/frontend/src/app/layout.tsx
@@ -43,32 +43,8 @@ export default function RootLayout({
                     authProvider={authProvider}
                     resources={[
                       {
-                        name: "legal-assistant",
-                        list: "/legal-assistant",
-                      },
-                      {
-                        name: "trivia",
-                        list: "/trivia",
-                      },
-                      {
-                        name: "blog_posts",
-                        list: "/blog-posts",
-                        create: "/blog-posts/create",
-                        edit: "/blog-posts/edit/:id",
-                        show: "/blog-posts/show/:id",
-                        meta: {
-                          canDelete: true,
-                        },
-                      },
-                      {
-                        name: "categories",
-                        list: "/categories",
-                        create: "/categories/create",
-                        edit: "/categories/edit/:id",
-                        show: "/categories/show/:id",
-                        meta: {
-                          canDelete: true,
-                        },
+                        name: "marketing-app",
+                        list: "/marketing",
                       }
                     ]}
                     options={{
