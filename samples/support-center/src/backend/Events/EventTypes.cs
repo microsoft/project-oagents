1e660cba185bc6f2248d6902c3edd1439a5030ab
Katarina Svedman <kapeltol@microsoft.com>
2024-05-22 16:07:31 +0200

    moved seed project due to conflict and added QnA functionality

--- samples/support-center/src/backend/Events/EventTypes.cs
+++ samples/support-center/src/backend/Events/EventTypes.cs
@@ -4,6 +4,8 @@ public enum EventTypes
 {
     CustomerInfoRequested,
     CustomerInfoRetrieved,
+    UserQuestionReceived,
+    UserQuestionAnswered,
     //UserConnected,
     //GraphicDesignCreated,
     //SocialMediaPostCreated,
