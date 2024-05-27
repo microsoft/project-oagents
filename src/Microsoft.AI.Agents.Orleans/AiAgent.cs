c0b6a7f8420fc978d5040173e804f643743133f2
Kosta Petan <kostapetan@gmail.com>
2024-05-27 09:26:46 +0000

    change max_tokens (output of model) to 4096

--- src/Microsoft.AI.Agents.Orleans/AiAgent.cs
+++ src/Microsoft.AI.Agents.Orleans/AiAgent.cs
@@ -45,7 +45,8 @@ public abstract class AiAgent<T> : Agent, IAiAgent where T : class, new()
 
     public virtual async Task<string> CallFunction(string template, KernelArguments arguments, OpenAIPromptExecutionSettings? settings = null)
     {
-        var propmptSettings = (settings == null) ? new OpenAIPromptExecutionSettings { MaxTokens = 18000, Temperature = 0.8, TopP = 1 }
+        // TODO: extract this to be configurable
+        var propmptSettings = (settings == null) ? new OpenAIPromptExecutionSettings { MaxTokens = 4096, Temperature = 0.8, TopP = 1 }
                                                 : settings;
         var function = _kernel.CreateFunctionFromPrompt(template, propmptSettings);
         var result = (await _kernel.InvokeAsync(function, arguments)).ToString();
