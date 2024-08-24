cf83b556c0611ba4e97737a56f03d92efcc8f5ba
Carlos Garcia Lalicata <crgarcia@live.com.ar>
2024-08-25 00:57:13 +0200

    adding dalle-3 noxt config

--- samples/marketing/src/frontend/next.config.mjs
+++ samples/marketing/src/frontend/next.config.mjs
@@ -10,6 +10,12 @@ const nextConfig = {
         port: '**',
         pathname: '**',
       },
+      {
+        protocol: 'https',
+        hostname: 'dalleprodsec.blob.core.windows.net',
+        port: '**',
+        pathname: '**',
+      },
     ],
   },
 };
