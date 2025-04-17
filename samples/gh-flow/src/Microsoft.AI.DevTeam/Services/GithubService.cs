635e73d8131dbfc05c199e703decd5fec616909f
Vincent Giraud <vgiraud@microsoft.com>
2025-04-17 18:31:26 +0200

    Fixed github issue type

--- samples/gh-flow/src/Microsoft.AI.DevTeam/Services/GithubService.cs
+++ samples/gh-flow/src/Microsoft.AI.DevTeam/Services/GithubService.cs
@@ -171,7 +171,7 @@ public class GithubService : IManageGithub
     {
         try
         {
-            await _ghClient.Issue.Comment.Create(org, repo, (int)issueNumber, comment);
+            await _ghClient.Issue.Comment.Create(org, repo, issueNumber, comment);
         }
         catch (Exception ex)
         {
